a367ca86f02daa1bf94f8a7a39708bbd083d1220
SeonyoungHa <syh5004@naver.com>
2021-07-31 15:49:25 +0900

    .

--- review-lessons/app/src/main/java/com/javajungsuk_basic/exercise/ch06/Ex6_1.java
+++
@@ -1,7 +0,0 @@
-package com.javajungsuk_basic.exercise.ch06;
-
-public class SutdaCard {
-	int num;
-	boolean isKwang;
-
-}
--- review-lessons/app/src/main/java/com/javajungsuk_basic/exercise/ch06/prac6_1_2.java
+++ review-lessons/app/src/main/java/com/javajungsuk_basic/exercise/ch06/prac6_1_2.java
@@ -1,8 +1,10 @@
 package com.javajungsuk_basic.exercise.ch06;
-class SutdaCard {
+class prac6_1_2 {
 	public static void main(String args[]) {
 		SutdaCard card1 = new SutdaCard(3, false);
 		SutdaCard card2 = new SutdaCard();
+		SutdaCard card3 = new SutdaCard(10);
+
 	}
 }
 
@@ -17,6 +19,10 @@ class SutdaCard{
 	SutdaCard(int num, boolean isKwang){
 		this.num = num;
 		this.isKwang = isKwang;
+	}
+
+	SutdaCard(int num){
+		this.num = num;
 	}
 
 	String info() {
