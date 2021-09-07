86ea5f7c24477f6815afcb350d0bb1dba8d0c20d
SeonyoungHa <syh5004@naver.com>
2021-09-07 17:35:57 +0900

    .

---
+++ review-lessons/app/src/main/java/programmers/Week1.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class Week1 {
+
+	public static void main(String[] args){
+		int price = 10 * count;
+		int money = 100;
+
+		for(int count = 0; 0 < count < 100; count++ ) {
+		}
+			
+			
+		if (money < price) {
+			System.out.println(i*price-money);
+		} else (money > 0){
+			System.out.println(0);
+		}
+	}
+
+}
